e67679a7db334b7729d9cbbd45bc426ed8cbeb9a
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-04-09 12:31:25 +0300

    cleaned up the code somewhat

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os, ntpath, shutil
+
 def AddHtmlToGit():
     print("making the git commit")
-    GaitCommitMessage = "Updated the HTML from live now here: https://*.pages.dev/index.html"
+    GitCommitMessage = "Updated the HTML from live now here: https://*.pages.dev/index.html"
     ntpath.realpath = ntpath.abspath
     shutil.copy("example.html", "prod-cf/index.html")
     print(os.system("git add ."))
     print(os.system("git commit -am "+chr(34)+GitCommitMessage+chr(34)))
     print(os.system("git push"))
-    pass
+
